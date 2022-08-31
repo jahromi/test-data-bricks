696181423940bdf121f333336e491770c6368f20
jahromi@outlook.com <jahromi@outlook.com>
2022-08-31 15:09:57 +0000

    test

--- test.py
+++ test.py
@@ -46,4 +46,73 @@ display(df1)
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC SELECT * FROM default.people10m TIMESTAMP AS OF current_timestamp()
+# MAGIC describe default.people10m
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC delete from default.people10m 
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from default.people10m TIMESTAMP AS OF '2023-08-29 23:20:21'
+
+# COMMAND ----------
+
+import pyspark
+from pyspark.sql import SparkSession
+SparkSession\
+.builder\
+.master("local")\
+.appName("spark session")\
+.config("spark.databricks.delta.retentionDurationCheck.enabled", "false")\
+.getOrCreate()
+
+# COMMAND ----------
+
+checkpoint_path = 's3://data-brick-test-inovation-week/auto-load-target/_checkpoints'
+write_path = 's3://data-brick-test-inovation-week/auto-load-target'
+upload_path = 's3://data-brick-test-inovation-week/auto-load-source'
+
+# Set up the stream to begin reading incoming files from the
+# upload_path location.
+df = spark.readStream.format('cloudFiles') \
+  .option('cloudFiles.format', 'csv') \
+  .option('header', 'true') \
+  .schema('city string, year int, population long') \
+  .load(upload_path)
+
+# Start the stream.
+# Use the checkpoint_path location to keep a record of all files that
+# have already been uploaded to the upload_path location.
+# For those that have been uploaded since the last check,
+# write the newly-uploaded files' data to the write_path location.
+df.writeStream.format('delta') \
+  .option('checkpointLocation', checkpoint_path) \
+  .start(write_path)
+
+# COMMAND ----------
+
+spark.sql("CREATE TABLE default.city_population USING DELTA LOCATION 's3://data-brick-test-inovation-week/auto-load-target'")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from default.city_population version as of 5
+
+# COMMAND ----------
+
+import boto3
+s3 = boto3.client('s3')
+response = s3.list_buckets()
+print(response)
+
+# COMMAND ----------
+
+import dlt
+
+@dlt.table
+def filtered_data():
+  return dlt.read("taxi_raw").where(...)
+
